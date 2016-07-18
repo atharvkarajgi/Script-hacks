1d167c33069bf00f467e6e2da273f7cb29b087de
karajrish <rishabh.karajgi@gmail.com>
2016-07-18 16:11:48 +0530

    pushed git hack

---
+++ bash/autogit.sh
@@ -0,0 +1,11 @@
+#checks if all arguments are provided arg1=repository name; arg2=commit message
+if [ $# -ne 2 ]; then
+    echo "Usage: $0 <directory name> <commit message>"
+    exit 1
+fi
+cd $1
+#stores username and password for bypassing authentication (1hr=3600s)
+git config --global credential.helper 'cache --timeout=36000'
+git add .
+git commit -m $2
+git push origin master
